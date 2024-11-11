498864c3a5c0eeaa9e06f05d8e6de83fa5272f4a
4KT <4KT@DESKTOP-J0KOTS8>
2024-11-11 02:04:06 +0000

    Add project files.

---
+++ Задача-1/Задача-1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+        int rows;
+        
+        scanf_s("%d", &rows);
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                printf("%d ", j);
+            }
+            printf("\n");
+        }
+
+        printf("\n");
+
+        for (int i = rows; i >= 1; --i) {
+            for (int j = 1; j <= i; ++j) {
+                printf("%d ", j);
+            }
+            printf("\n");
+        }
+}
+
